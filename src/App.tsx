1ec734889a982401d9bbfcb67f4f98cf3d057ceb
PhamDucPhuongMinh <phamducphuongminh0103@gmail.com>
2023-04-18 15:35:43 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.scss";
+
+// Reset CSS
+import "normalize.css";
+
+// CSS Swipper
+import "swiper/css/bundle";
+
+// CSS Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// CSS Toast
+import "react-toastify/dist/ReactToastify.css";
+
+const App: React.FC = () => {
+  return <div className="App"></div>;
+};
+
+export default App;
